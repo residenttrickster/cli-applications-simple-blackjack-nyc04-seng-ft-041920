85485d0baaca474b627c711aeadd3404def59744
Anastasia Orlova <metkusblackus@gmail.com>
2020-05-11 01:54:40 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -63,10 +63,10 @@ end
 def runner
   # code runner here
   welcome
-  card_total = initial_round
-  while card_total < 21
-    card_total = hit?(card_total)
-    display_card_total(card_total)
+  total = initial_round
+  while total < 21
+    total = hit?(total)
+    display_card_total(total)
   end
-end_game(card_total)  
+end_game(total)  
 end
